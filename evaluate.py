809f351f84ed81be938a612789fe5e7bbd39338b
ONKAR Susladkar <46415298+Onkarsus13@users.noreply.github.com>
2022-08-01 20:12:13 +0530

    Add files via upload

--- evaluate.py
+++ evaluate.py
@@ -3,17 +3,18 @@ import torch
 from model import get_model_loss_optimizer
 from dataset import get_loader
 import config as cfg
-import tqdm as tqdm
+from tqdm import tqdm
 
 
 def f1(cfg):
 
     device = cfg.device
 
-    test_loader = get_loader(cfg.test_path, cfg.batch_size, cfg.num_worker, cfg.shuffle)
+    test_loader = get_loader(cfg.test_path, cfg.max_len, cfg.batch_size, cfg.num_worker, cfg.shuffle)
 
     model, _, _ = get_model_loss_optimizer(cfg.lr, cfg.device)
     model.load_state_dict(torch.load(cfg.model_path + 'model.pth')['state_dict'])
+    print('model is loaded ...')
 
     final_f1 = []
 
@@ -31,9 +32,12 @@ def f1(cfg):
 
             pred1, pred2, pred3 = model(x2, x3, x1)
 
-            f1_1 = torchmetrics.F1Score(num_classes=pred1.shape[-1])(pred1, y1)
-            f1_2 = torchmetrics.F1Score(num_classes=pred2.shape[-1])(pred2, y2)
-            f1_3 = torchmetrics.F1Score(num_classes=pred3.shape[-1])(pred3, y3)
+            f1_1 = torchmetrics.F1Score(num_classes=pred1.shape[-1]).to(device)
+            f1_1 = f1_1(pred1, y1)
+            f1_2 = torchmetrics.F1Score(num_classes=pred2.shape[-1]).to(device)
+            f1_2 = f1_2(pred2, y2)
+            f1_3 = torchmetrics.F1Score(num_classes=pred3.shape[-1]).to(device)
+            f1_3 = f1_3(pred3, y3)
 
             f1 = (f1_1 + f1_2 + f1_3)/3
 
@@ -45,4 +49,4 @@ def f1(cfg):
 
 if __name__ == "__main__":
     
-    print('Test F1 ', f1(cfg))
+    print('Test F1 ', f1(cfg).item())
